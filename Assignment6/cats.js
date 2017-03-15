a984e658941a18af595ad5d4a9e64a134b336fa1
JrNordell <nordejr@gmail.com>
2017-03-15 12:11:54 -0500

    Working on Assignment 6

--- Assignment6/cats.js
+++ Assignment6/cats.js
@@ -1,3 +1,8 @@
 function swapImage(id, pathname){
-    document.getElementById(id).setAttribute('src', pathname);
+    document.getElementById(id).src = pathname;
+}
+
+function playAudio(pathName){
+    var audioBit = new Audio('audio/cat_meow.mp3');
+    audioBit.play();
 }
