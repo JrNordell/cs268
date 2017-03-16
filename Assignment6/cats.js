9afb57634222a966c3c9061c41e35f18851c6c4b
JrNordell <nordejr@gmail.com>
2017-03-16 11:33:14 -0500

    Working on Assignment 6

--- Assignment6/cats.js
+++ Assignment6/cats.js
@@ -2,7 +2,22 @@ function swapImage(id, pathname){
     document.getElementById(id).src = pathname;
 }
 
-function playAudio(pathName){
-    var audioBit = new Audio('audio/cat_meow.mp3');
-    audioBit.play();
+function playAudio(id){
+   var aud = document.getElementById(id);
+   aud.play();
+}
+
+function makeClear(bool, pathname){
+    if(bool == 1) {
+        document.getElementById("container").style.opacity = .2;
+    }else{
+        document.getElementById("container").style.opacity = 1;
+    }
+    document.getElementById("empty").src = pathname;
+    document.getElementById("popupContainer").style.visibility = 'visible';
+    document.getElementById("popupContainer").clientTop = window.screenTop;
+}
+
+function popupImage(){
+
 }
