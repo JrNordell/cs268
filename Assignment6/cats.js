24baf4fece7a80e2100f2dfcd93d42aa754dbcbc
JrNordell <nordejr@gmail.com>
2017-03-16 23:54:01 -0500

    'Finished' Assignment 6

--- Assignment6/cats.js
+++ Assignment6/cats.js
@@ -1,3 +1,5 @@
+
+
 function swapImage(id, pathname){
     document.getElementById(id).src = pathname;
 }
@@ -10,14 +12,45 @@ function playAudio(id){
 function makeClear(bool, pathname){
     if(bool == 1) {
         document.getElementById("container").style.opacity = .2;
+        document.getElementById("popupContainer").style.visibility = 'visible';
+        // // var a = (window.innerHeight - document.getElementById("popupContainer").offsetHeight)/2;
+        // // var b = (window.innerWidth - document.getElementById("popupContainer").offsetWidth)/2;
+        // var a = ($(window).height()) - ($("popupContainer").offsetHeight);
+        // var b = ($(window).width()) - ($("popupContainer").offsetWidth);
+        // document.getElementById("popupContainer").style.top = a;
+        // document.getElementById("popupContainer").style.left = b;
+        // console.log(a, document.getElementById("popupContainer").style.top, b);
+
+        //Javascript was not changing style attributes for some unknown reason.
+        //switched to Jquery because I couldn't figure anything else out
+
+
+
+        $('#popupContainer').css("top", (($(window).height() - 333)/2) + $(window).scrollTop() + "px");
+        $('#popupContainer').css("left", (($(window).width() - 500)/2) + $(window).scrollLeft() + "px");
     }else{
         document.getElementById("container").style.opacity = 1;
+        document.getElementById("popupContainer").style.visibility = 'hidden';
     }
     document.getElementById("empty").src = pathname;
-    document.getElementById("popupContainer").style.visibility = 'visible';
-    document.getElementById("popupContainer").clientTop = window.screenTop;
+    document.getElementById("vidempty").src = "";
 }
 
-function popupImage(){
+//if you double click the video it will go to the center, unknown why this is the case
+function popupVideo(bool, pathname){
+    if(bool == 1){
+        document.getElementById("container").style.opacity = .2;
+        document.getElementById("popupContainer").style.visibility = 'visible';
 
+        //same reason as above for JQuery use here
+
+        $('#popupContainer').css("top", (($(window).height() - $('#popupContainer').outerHeight())/2) + $(window).scrollTop() + "px");
+        $('#popupContainer').css("left", (($(window).width() - $('#popupContainer').outerWidth())/2) + $(window).scrollLeft() + "px");
+    }else{
+        document.getElementById("container").style.opacity = 1;
+        document.getElementById("popupContainer").style.visibility = 'hidden';
+    }
+    document.getElementById("vidempty").src = pathname;
+    document.getElementById("empty").src = "";
 }
+
